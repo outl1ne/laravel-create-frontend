d72edd320c873c6bf739b4bd82230f87a214b13e
Allan Tatter <allan.tatter@gmail.com>
2018-01-24 16:33:43 +0200

    Initial commit

---
+++ src/helpers.php
@@ -0,0 +1,31 @@
+<?php
+
+if (! function_exists('frontend')) {
+    /**
+     * Get the path to a Create Frontend manifest file.
+     *
+     * @param  string  $path
+     * @param  string  $manifestPath
+     * @return string
+     *
+     * @throws \Exception
+     */
+    function frontend($path, $manifestPath = 'asset-manifest.json')
+    {
+        static $manifests = [];
+
+        $manifestPath = base_path($manifestPath);
+
+        if (! isset($manifests[$manifestPath])) {
+            if (! file_exists($manifestPath)) {
+                throw new Exception('The Create Frontend manifest does not exist.');
+            }
+
+            $manifests[$manifestPath] = json_decode(file_get_contents($manifestPath), true);
+        }
+
+        $manifest = $manifests[$manifestPath];
+
+        return $manifest[$path] ?? false;
+    }
+}
